c23f553f03091eeec2209ebddc0a59492ca8fdd5
giannibaccino <giannibaccino@gmail.com>
2022-07-01 07:25:22 -0300

    Se agregan comandos de movimiento para las barras pero aun no se ve el movimiento

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-(function(){
+(function(){ //Se implementa el tablero
     self.Board = function(width, height){
         this.width = width;
         this.height = height;
@@ -17,7 +17,7 @@
     }
 })();
 
-(function(){
+(function(){ //Se implemnenta la barra de juego
     self.Bar = function(x,y,width,height,board){
         this.x = x;
         this.y = y;
@@ -26,19 +26,23 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed = 10;
     }
 
     self.Bar.prototype = {
         down: function(){
-
+            this.y += this.speed;
         },
         up: function(){
-
+            this.y -= this.speed;
+        },
+        toString: function(){
+            return "x: " + this.x + " y: " + this.y;
         }
     }
 })();
 
-(function(){
+(function(){ //Se implementa la vista del tablero
     self.BoardView = function(canvas, board){
         this.canvas = canvas;
         this.canvas.width = board.width;
@@ -48,7 +52,7 @@
     }
 
     self.BoardView.prototype = {
-        draw: function(){
+        draw: function(){ //Dibuja el tablero
             for(var i = this.board.elements.length - 1; i >= 0; i--){
                 var el = this.board.elements[i];
                 draw(this.ctx, el);
@@ -67,14 +71,29 @@
     }
 })();
 
-window.addEventListener("load",main);
+var board = new Board(800,400);
+var bar = new Bar(20,140,40,100,board);
+var bar2 = new Bar(740,140,40,100,board);
+var canvas = document.getElementById("canvas");
+var board_view = new BoardView(canvas, board);
 
-function main(){
-    var board = new Board(800,400);
-    var bar = new Bar(20,140,40,100,board);
-    var bar = new Bar(740,140,40,100,board);
-    var canvas = document.getElementById("canvas");
-    var board_view = new BoardView(canvas, board);
+document.addEventListener("keydown", function(ev) {
+    if(ev.keyCode == 38)
+        bar.up();
+    else if(ev.keyCode == 40)
+        bar.down();
+    else if(ev.keyCode == 87)
+        bar2.up();
+    else if (ev.keyCode == 83)
+        bar2.down();
+
+    console.log(bar.toString());
+    console.log(bar2.toString());
+});
+
+self.addEventListener("load",main);
+
+function main(){ //Se inicia un juego    
     console.log(board);
     board_view.draw();
 }
